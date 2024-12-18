aa5bb3aa683209b28fd1d1cdb7c02b3f2c6d5939
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-12-18 15:05:36 +0200

    refactored ctrls add post patch delete

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,34 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+export const createContact = async (contactData) => {
+  const newContact = await ContactsCollection.create(contactData);
+  return newContact;
+};
+
+export const updateContact = async (
+  contactId,
+  updateContactData,
+  options = {},
+) => {
+  const result = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    updateContactData,
+    {
+      new: true,
+      includeResultMetadata: true,
+    },
+  );
+
+  if (!result || !result.value) return null;
+
+  return {
+    contact: result.value,
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findOneAndDelete({ _id: contactId });
+  return contact;
+};
