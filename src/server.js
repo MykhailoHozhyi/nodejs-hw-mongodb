ba5c1f5eae85449db1d04559145e4c715c2dcdd7
Mykhailo Hozhyi <gozhenky@gmail.com>
2025-01-20 21:27:44 +0200

    add swagger

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -24,6 +25,8 @@ export const setupServer = () => {
       },
     }),
   );
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use(router);
 
